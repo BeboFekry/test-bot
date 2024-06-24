de39a9513b2ef8462e91355a1d666d186fd27c91
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-06-24 14:02:52 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask, request, jsonify
+import numpy as np
+import tensorflow as tf
+# from tf.keras.models import load_model
+from tf.keras.utils import img_to_array
+
+url = "https://drive.google.com/uc?id=1MTntYoyzv_Y2veMiC90eQqwF8m7GYJmM"
+model = requests.get(url)
+
+def rescale(img):
+    img2 = img_to_array(img)
+    img2 = img2/255
+    img2 = img2.reshape(1,224,224,3)
+    return img2
+
+def pred(model,img):
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
+    model.set_tensor(input_details[0]['index'],img)
+    model.invoke()
+    output_data = model.get_tensor(output_details[0]['index'])
+    return output_data
+
+app = Flask(__name__)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    img = request.files['file']
+    img = rescale(img)
+    output = pred(model,img).argmax()
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
