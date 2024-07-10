0d7d5dd3f1c8e133ac8b9fabc6e61ae7c4ea911e
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-10 16:32:33 +0300

    Update home.py

--- home.py
+++ home.py
@@ -1,32 +1,11 @@
 import streamlit as st
 # st.logo("images/Logo.png")
-st.image("images/background.png")
-st.title("I CARE")
-st.info("Easy Healthcare for Anyone Anytime")
-st.write("""You can access a three main modules:
 
-Module 1: An AI based smart chatbot called "Caroline" talking to the patient 
-and taking its disease symptoms, then diagnosing the disease and 
-recommend making some tests as x-ray, MRI ... in addition, given 
-information about the predicted disease as an overview, symptoms, and 
-treatments. 
-It can predict 30 diseases such as (Breast Cancer, Influenza, Covid 19, Stroke, ...) 
-
-Module 2: A sequence of AI Computer Vision models for scanning medical 
-imaging and tests it can scan (X-ray, MRI, CT, OCT, ECG, or Food image), 
-detect the image type (Image Recognition), if it is medical imaging image, 
-applying anatomical recognition, disease evaluation, disease diagnosis. It 
-can predict 25 disease types such as (Bone Fracture, Brain Tumor, Covid 19, 
-Breast Cancer, ...) 
-It also can recognize 101 food types from images and shows the approximate number of calories per gram.
-
-Module 3: An ensemble Machine Learning (Random Forest) Model for scan Electrocardiography ECG and diagnosis the heart diseases.
-""")
-
+home = st.Page("home.py", title="Home", icon=":material/home:", default=True)
 chat = st.Page("chatbot.py", title="Chatbot", icon=":material/smart_toy:")
 scan = st.Page("scan.py", title="Scan", icon=":material/radiology:")
 ecg = st.Page("ecg.py", title="ECG Scan", icon=":material/ecg_heart:")
 mbti = st.Page("mbti.py", title="MBTI", icon=":material/psychology:")
 
-pg = st.navigation([chat, scan, ecg, mbti])
+pg = st.navigation([home, chat, scan, ecg, mbti])
 pg.run()
